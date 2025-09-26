df842f3e0bac74ef48754781b85f095937a0f191
Nimrod-ogoro <nimrodogoro@gmail.com>
2025-09-26 15:37:17 +0300

    commit

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -6,7 +6,7 @@ import Contact from '@/components/Contact';
 import Footer from '@/components/Footer';
 import Portfolio from '@/components/Portfolio';
 import WhatsAppFloat from '@/components/WhatsAppFloat';
-import { Helmet } from "react-helmet-async";
+
 
 
 const Index = () => {
@@ -15,10 +15,7 @@ const Index = () => {
     
    
     <div className="min-h-screen">
-      <Helmet>
-            <meta name="google-site-verification" content="0YS0oTy2nfF5h2AVwaXy9yP-XGnRipda0ItQ3fiwl0U" />
-        <title>LD Events</title>
-      </Helmet>
+      
       <Header />
       <main>
         <Hero />
